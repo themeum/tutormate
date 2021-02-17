61af331a342e000aa697a523046ce1b4f45383b9
iamsaief <saiefalemon@gmail.com>
2021-02-18 00:15:13 +0600

    circle-progressbar aaded

--- assets/js/demo-importer.js
+++ assets/js/demo-importer.js
@@ -554,11 +554,30 @@ function App() {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _ProgressBar__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ProgressBar */ "./react/src/components/ProgressBar.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var useState = wp.element.useState;
 
 
 var Installation = function Installation(_ref) {
   var status = _ref.status,
       percentage = _ref.percentage;
+
+  var _useState = useState(70),
+      _useState2 = _slicedToArray(_useState, 2),
+      circleProgress = _useState2[0],
+      setCircleProgress = _useState2[1];
+
   return /*#__PURE__*/React.createElement("div", {
     className: "installation-screen modal-wrapper active"
   }, /*#__PURE__*/React.createElement("div", {
@@ -582,16 +601,68 @@ var Installation = function Installation(_ref) {
   }, percentage, "%")), /*#__PURE__*/React.createElement("div", {
     className: "plugin-status"
   }, /*#__PURE__*/React.createElement("div", {
-    className: "circle-progress"
-  }, /*#__PURE__*/React.createElement("span", {
-    className: "progress-circle"
-  }), /*#__PURE__*/React.createElement("span", {
-    className: "progress-bg"
-  }), /*#__PURE__*/React.createElement("span", {
-    className: "progress-complete"
+    className: "plugin-item"
+  }, circleProgress < 100 ? /*#__PURE__*/React.createElement("svg", {
+    id: "svg-circle"
+  }, /*#__PURE__*/React.createElement("circle", {
+    className: "bg",
+    cx: "7",
+    cy: "7",
+    r: "7"
+  }), /*#__PURE__*/React.createElement("circle", {
+    style: {
+      strokeDashoffset: "calc(var(--dashArr) - (var(--dashArr) * ".concat(circleProgress, ") / 100)")
+    },
+    className: "fg",
+    cx: "7",
+    cy: "7",
+    r: "7"
+  })) : /*#__PURE__*/React.createElement("svg", {
+    id: "svg-circle"
+  }, /*#__PURE__*/React.createElement("circle", {
+    className: "circle-full",
+    cx: "7",
+    cy: "7",
+    r: "7",
+    fill: "#5FAC23"
+  }), /*#__PURE__*/React.createElement("path", {
+    className: "check-mark",
+    d: "M6.138 8.9714L3.9427 6.776 3 7.7187l3.138 3.138L12 4.9427l-.9427-.9426L6.138 8.9714z",
+    fill: "#fff"
   })), /*#__PURE__*/React.createElement("div", {
     className: "title"
-  }, "Tutor LMS")))));
+  }, "Tutor LMS")), /*#__PURE__*/React.createElement("div", {
+    className: "plugin-item"
+  }, circleProgress < 100 ? /*#__PURE__*/React.createElement("svg", {
+    id: "svg-circle"
+  }, /*#__PURE__*/React.createElement("circle", {
+    className: "bg",
+    cx: "7",
+    cy: "7",
+    r: "7"
+  }), /*#__PURE__*/React.createElement("circle", {
+    style: {
+      strokeDashoffset: "calc(var(--dashArr) - (var(--dashArr) * ".concat(circleProgress, ") / 100)")
+    },
+    className: "fg",
+    cx: "7",
+    cy: "7",
+    r: "7"
+  })) : /*#__PURE__*/React.createElement("svg", {
+    id: "svg-circle"
+  }, /*#__PURE__*/React.createElement("circle", {
+    className: "circle-full",
+    cx: "7",
+    cy: "7",
+    r: "7",
+    fill: "#5FAC23"
+  }), /*#__PURE__*/React.createElement("path", {
+    className: "check-mark",
+    d: "M6.138 8.9714L3.9427 6.776 3 7.7187l3.138 3.138L12 4.9427l-.9427-.9426L6.138 8.9714z",
+    fill: "#fff"
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "title"
+  }, "Tutor LMS"))))));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Installation);
--- react/src/components/Installation.js
+++ react/src/components/Installation.js
@@ -1,6 +1,9 @@
+const { useState } = wp.element;
 import ProgressBar from "./ProgressBar";
 
 const Installation = ({status, percentage}) => {
+    const [circleProgress, setCircleProgress] = useState(70);
+
     return (
         <div className="installation-screen modal-wrapper active">
             <div className="modal-content">
@@ -15,20 +18,34 @@ const Installation = ({status, percentage}) => {
                         <div className="percentage">{percentage}%</div>
                     </div>
                     <div className="plugin-status">
-                        <div className="circle-progress">
-                            {/* <svg width="14" height="14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path id="progress-bg" d="M7 14c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7z" fill="#E3E7EC"/>
-                                <path id="progress-circle" d="M7 0A7 7 0 00.0922 8.1326L7 7V0z" fill="#5FAC23"/>
-                                <path id="progress-fg" d="M7 12.25c2.8995 0 5.25-2.3505 5.25-5.25S9.8995 1.75 7 1.75 1.75 4.1005 1.75 7 4.1005 12.25 7 12.25z" fill="#fff"/>
-                                <circle id="complete-circle" cx="7" cy="7" r="7" fill="#5FAC23"/>
-                                <path id="check-mark" d="M6.138 8.9714L3.9427 6.776 3 7.7187l3.138 3.138L12 4.9427l-.9427-.9426L6.138 8.9714z" fill="#fff"/>
-                            </svg> */}
-                            <span className="progress-circle"></span>
-                            <span className="progress-bg"></span>
-                            <span className="progress-complete"></span>
-                            
+                        <div className="plugin-item">
+                            {
+                                circleProgress < 100 ? 
+                                <svg id="svg-circle">
+                                    <circle className="bg" cx="7" cy="7" r="7"></circle>
+                                    <circle style={{strokeDashoffset: `calc(var(--dashArr) - (var(--dashArr) * ${circleProgress}) / 100)`}} className="fg" cx="7" cy="7" r="7"></circle>
+                                </svg> : 
+                                <svg id="svg-circle">
+                                    <circle className="circle-full" cx="7" cy="7" r="7" fill="#5FAC23"></circle>
+                                    <path className="check-mark" d="M6.138 8.9714L3.9427 6.776 3 7.7187l3.138 3.138L12 4.9427l-.9427-.9426L6.138 8.9714z" fill="#fff"></path>
+                                </svg>
+                            }
+                            <div className="title">Tutor LMS</div>
                         </div>
-                        <div className="title">Tutor LMS</div>
+                        <div className="plugin-item">
+                            {
+                                circleProgress < 100 ? 
+                                <svg id="svg-circle">
+                                    <circle className="bg" cx="7" cy="7" r="7"></circle>
+                                    <circle style={{strokeDashoffset: `calc(var(--dashArr) - (var(--dashArr) * ${circleProgress}) / 100)`}} className="fg" cx="7" cy="7" r="7"></circle>
+                                </svg> : 
+                                <svg id="svg-circle">
+                                    <circle className="circle-full" cx="7" cy="7" r="7" fill="#5FAC23"></circle>
+                                    <path className="check-mark" d="M6.138 8.9714L3.9427 6.776 3 7.7187l3.138 3.138L12 4.9427l-.9427-.9426L6.138 8.9714z" fill="#fff"></path>
+                                </svg>
+                            }
+                            <div className="title">Tutor LMS</div>
+                        </div>
                     </div>
                 </div>
             </div>
